47c0f9e1bc8b110ab62692f505256b367e6f4a47
Artem <temich12335@gmail.com>
2019-06-11 16:37:23 +0300

    first-commit

---
+++ menu.js
@@ -0,0 +1,5 @@
+	$( document ).ready(function(){
+	  $( ".search-img" ).click(function(){ // задаем функцию при нажатиии на элемент с классом toggle
+	    $( ".form-search" ).toggle(); //  скрываем, или отображаем все элементы <div>
+	  });
+	});
